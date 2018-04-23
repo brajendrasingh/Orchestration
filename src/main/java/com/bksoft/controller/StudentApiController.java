934bdd2c43a729744e0f15e2fbb6abb2a5aaf955
brajendrasingh <brajendrasingh3clogic@gmail.com>
2018-04-23 23:06:12 +0530

    updated pom.xml

--- src/main/java/com/bksoft/controller/StudentApiController.java
+++ src/main/java/com/bksoft/controller/StudentApiController.java
@@ -6,8 +6,8 @@ package com.bksoft.controller;
 import com.bksoft.api.StudentApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
